d6c57085e7e3aa5ff737033b938cb840977a8ca9
Mateusz Slyszewski <mateuszslyszewski@gmail.com>
2020-05-22 16:48:22 +0200

    practice

---
+++ 3-Komponenty-wprowadzenie/practice/app.js
@@ -0,0 +1,34 @@
+// komponent funkcyjny, bezstanowy (stateless)
+
+const Header = () => {
+  return <h1>Welcome to the site</h1>
+}
+
+// komponent klasowy,stanowy (stateful)
+
+class Article extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <section>
+
+        <h2>Artykuł</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint quia beatae consequatur a, modi voluptatibus repudiandae. Optio rem hic quam, ducimus esse qui, itaque, suscipit nemo odio commodi odit aspernatur.</p>
+      </section>
+    )
+  }
+}
+
+const Blog = () => {
+  return (
+    <>
+      <Header />
+      <Article />
+    </>
+  )
+}
+
+ReactDOM.render(<Blog />, document.getElementById('root'));
